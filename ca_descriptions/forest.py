2f7038d0db3cc12a43634a0bd7d38f892fd9cc4e
Aydin Hepsaydir <arphepsaydir1@sheffield.ac.uk>
2018-03-01 21:02:19 +0000

    added flamability to forest and canyon, also added decay period

--- ca_descriptions/forest.py
+++ ca_descriptions/forest.py
@@ -34,26 +34,26 @@ def setup(args):
     # 4 Village
     # 5 Fire
     # 6 Burnt out
-    
-	#config.num_generations = 100
+
+	# config.num_generations = 100
     # -------------------------------------------------------------------------
 
     # ---- Override the defaults below (these may be changed at anytime) ----
-	
+
     config.state_colors = [(0.5,0.9,0.1),(0,0,1),(0.4,0.4,0.4), (0.4,0.6,0.37),(0,0,0), (1,0,0),(0.4,0,0)]
     config.grid_dims = (50,50)
     config.initial_grid = np.zeros(config.grid_dims)
-    config.initial_grid[5:6,5:6] = 5
+    config.initial_grid[5:8,5:8] = 5
     config.initial_grid[10:15, 5:15] = 1
     config.initial_grid[5:35, 32:35] = 2
     config.initial_grid[30:41, 15:25] = 3
     config.initial_grid[48:50, :3] = 4
     config.wrap = False
-	
+
 	#set_grid_dims(dims = (200, 200))
     #set_initial_grid(grid)
 	#config.inital_grid[1,1] = 1
-	
+
     # ----------------------------------------------------------------------
 
     # the GUI calls this to pass the user defined config
@@ -65,18 +65,33 @@ def setup(args):
     return config
 
 
-def transition_function(grid, neighbourstates, neighbourcounts):
+def transition_function(grid, neighbourstates, neighbourcounts, decaygrid):
     """Function to apply the transition rules
     and return the new grid"""
-    # Make cells on fire
+
+    # Find cells in certain states
     cell_in_state_0 = (grid == 0)
-    three_5_neighbours = (neighbourcounts[5] >= 1)
-    to_5 = cell_in_state_0 & three_5_neighbours
-    
-    #Make cells burnt out
+    cell_in_state_2 = (grid == 2)
+    cell_in_state_3 = (grid == 3)
+
+    # Find neighbours to these cells
+    three_5_neighbours = (neighbourcounts[5] >= 3)
+    one_5_neighbour = (neighbourcounts[5] >= 1)
+    four_5_neighbours = (neighbourcounts[5] >= 4)
+
+    # Make them Burn!!!
+    to_5 = (cell_in_state_0 & three_5_neighbours) | (cell_in_state_2 & one_5_neighbour) | (cell_in_state_3 & four_5_neighbours)
+
+    # Decay
     cell_in_state_5 = (grid == 5)
-    grid[cell_in_state_5] = 6
+
+    # Minus one from burning cell count until it reaches 0
+    decaygrid[cell_in_state_5] -= 1
+    decayed_to_zero = (decaygrid == 0)
+
+    grid[decayed_to_zero] = 6
     grid[to_5] = 5
+
     return grid
 
 
@@ -85,16 +100,27 @@ def main():
     # Get the config object from set up
     config = setup(sys.argv[1:])
 
-    # Create grid object using parameters from config + transition function
-    grid = Grid2D(config, transition_function)
+    decaygrid = np.zeros(config.grid_dims)
 
-    # Run the CA, save grid state every generation to timeline
+    decaygrid.fill(5)
+
+    decaygrid[5:35, 32:35] = 1
+    decaygrid[30:41, 15:25] = 10
+
+
+
+    # Create grid object using parameters from config + transition function
+    grid = Grid2D(config, (transition_function, decaygrid))
+
+    # Run the CA, save grid state every generation to line
     timeline = grid.run()
 
     # Save updated config to file
     config.save()
+
     # Save timeline to file
     utils.save(timeline, config.timeline_path)
 
 if __name__ == "__main__":
+
     main()
